84cbee171fa7fbb33ba72cb6e1234830941002ed
Paul Smith <psmith@magna.co.uk>
2025-03-13 20:27:51 +0000

    Added splitting

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
             for shot in shots:
                 if asteroid.check_collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
